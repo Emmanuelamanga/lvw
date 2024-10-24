3b1ad2e620ccce8230a65bb3c71ea358fe16a129
Emmanuelamanga <Emmanuelamanga@users.noreply.github.com>
2024-10-24 12:15:06 +0000

    PHP Linting (Pint)

--- app/Livewire/Post.php
+++ app/Livewire/Post.php
@@ -6,84 +6,98 @@ use App\Models\Post as ModelsPost;
 use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
-use function Laravel\Prompts\select;
-
 class Post extends Component
 {
-    //variable declarations 
-    public $posts, $title, $description, $postId, $update_post = false, $add_post= false;
-    
-    // action listeners 
+    //variable declarations
+    public $posts;
+
+    public $title;
+
+    public $description;
+
+    public $postId;
+
+    public $update_post = false;
+
+    public $add_post = false;
+
+    // action listeners
     protected $listeners = [
-        'deletePostListener' => 'deletePost'
+        'deletePostListener' => 'deletePost',
     ];
 
-    // form rules 
+    // form rules
     protected $rules = [
         'title' => 'required',
-        'description' => 'required',        
+        'description' => 'required',
     ];
 
-    // reset field fuction 
-    public function resetFields(){
+    // reset field fuction
+    public function resetFields()
+    {
         $this->title = '';
         $this->description = '';
     }
-    
-    // render post data 
-    public function render() : View
+
+    // render post data
+    public function render(): View
     {
-        $this->posts= ModelsPost::select('id', 'title', 'description')->get();
+        $this->posts = ModelsPost::select('id', 'title', 'description')->get();
+
         return view('livewire.post');
     }
 
-    // add post form 
-    public function addPost(){
+    // add post form
+    public function addPost()
+    {
         $this->resetFields();
         $this->add_post = true;
         $this->update_post = false;
     }
-    
-    // store posts 
-    public function storePost(){
+
+    // store posts
+    public function storePost()
+    {
         $this->validate();
         try {
             ModelsPost::create([
                 'title' => $this->title,
-                'description'=>$this->description,
+                'description' => $this->description,
             ]);
-            // update session 
+            // update session
             session()->flash('success', 'Post Created Successfully!!');
             $this->resetFields();
             $this->add_post = false;
-            
+
         } catch (\Exception $th) {
             session()->flash('error', 'Something went wrong!!');
         }
     }
 
-    // show existing post 
-    public function editPost($id){
+    // show existing post
+    public function editPost($id)
+    {
         try {
-                $post = ModelsPost::findOrFail($id);
-                // @phpstan-ignore booleanNot.alwaysFalse
-                if (!$post) {
-                    session()->flash('error', 'Post Not Found!');
-                }else{
-                    $this->title=$post->title;
-                    $this->description=$post->description;
-                    $this->postId=$post->id;
-                    $this->update_post=true;
-                    $this->add_post=false;
-                    
-                }
-            } catch (\Throwable $th) {
-                session()->flash('error', 'Something Went Wrong!!');
-            }  
-    }   
-    
+            $post = ModelsPost::findOrFail($id);
+            // @phpstan-ignore booleanNot.alwaysFalse
+            if (! $post) {
+                session()->flash('error', 'Post Not Found!');
+            } else {
+                $this->title = $post->title;
+                $this->description = $post->description;
+                $this->postId = $post->id;
+                $this->update_post = true;
+                $this->add_post = false;
+
+            }
+        } catch (\Throwable $th) {
+            session()->flash('error', 'Something Went Wrong!!');
+        }
+    }
+
     // update post
-    public function updatePost(){
+    public function updatePost()
+    {
         $this->validate();
         try {
             ModelsPost::whereId($this->postId)->update([
@@ -93,25 +107,27 @@ class Post extends Component
             session()->flash('success', 'Post Updated Successfully');
             $this->resetFields();
             $this->update_post = false;
-            
+
         } catch (\Exception $th) {
             session()->flash('title', 'Something Went Wrong');
         }
     }
-    
-    // cancel add/edit form 
-    public function cancelForm(){
+
+    // cancel add/edit form
+    public function cancelForm()
+    {
         $this->add_post = false;
         $this->update_post = false;
         $this->resetFields();
     }
-    
-    public function deletePost($id){
+
+    public function deletePost($id)
+    {
         try {
             ModelsPost::find($id)->delete();
             session()->flash('success', 'Post Deleted Successfully');
         } catch (\Exception $th) {
-            session()->flash('error','Something Went Wrong!!');
+            session()->flash('error', 'Something Went Wrong!!');
         }
     }
 }
